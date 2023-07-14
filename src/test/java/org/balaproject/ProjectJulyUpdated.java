4a9383171522a4ec8240cbd1a280e8a1f70ef198
bala.anandha <bala.anandha@gmail.com>
2023-07-14 08:00:49 +0530

    Project Structure Explanantion Updated

---
+++ src/test/java/org/balaproject/ProjectJulyUpdated.java
@@ -0,0 +1,12 @@
+package org.balaproject;
+
+public class ProjectJulyUpdated {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Test 1");
+		System.out.println("Test 2");
+		System.out.println("Test 3");
+	}
+
+}
