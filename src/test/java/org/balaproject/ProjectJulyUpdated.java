12d4e38c7dfb43a45807fd3f3fec029c666979d1
bala.anandha <bala.anandha@gmail.com>
2023-07-14 17:53:53 +0530

    Balakumar's code

--- src/test/java/org/balaproject/ProjectJulyUpdated.java
+++ src/test/java/org/balaproject/ProjectJulyUpdated.java
@@ -7,6 +7,9 @@ public class ProjectJulyUpdated {
 		System.out.println("Test 1");
 		System.out.println("Test 2");
 		System.out.println("Test 3");
+		System.out.println("Test 4");
+		System.out.println("Test 5");
+		System.out.println("Test 6");
 	}
 
 }
